2e46a263f51f187646a81065564451ad98312825
Rafi Siddique <rafisiddique67@gmail.com>
2025-12-17 18:13:04 +0600

    [Done] test eslint, prettier and add husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,47 +1,52 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import jsdoc from "eslint-plugin-jsdoc";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import jsdoc from 'eslint-plugin-jsdoc'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-
-export default [{
-        files: ["**/*.{js,mjs,cjs,ts}"]
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+  },
+  eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.js'],
+    plugins: {
+      jsdoc: jsdoc,
     },
-    {
-        files: ["**/*.js"],
-        plugins: {
-            jsdoc: jsdoc
+    rules: {
+      'jsdoc/require-description': 'error',
+      'jsdoc/check-values': 'error',
+    },
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    rules: {
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'prefer-const': [
+        'error',
+        {
+          ignoreReadBeforeAssign: true,
         },
-        rules: {
-            "jsdoc/require-description": "error",
-            "jsdoc/check-values": "error"
-        },
-        languageOptions: {
-            sourceType: "commonjs"
-        }
+      ],
     },
-    {
-        languageOptions: {
-            globals: globals.browser
-        }
-    },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        languageOptions: {
-            globals: globals.browser
-        }
-    },
-    {
-        rules: {
-            eqeqeq: "off",
-            "no-unused-vars": "error",
-            "prefer-const": ["error", {
-                ignoreReadBeforeAssign: true
-            }],
-        },
-    },
-    {
-        ignores: [".node_modules/*"]
-    },
-];
+  },
+  {
+    ignores: ['.node_modules/*'],
+  },
+]
