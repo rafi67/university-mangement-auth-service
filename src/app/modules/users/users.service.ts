43c45f79a04761b7bc67befb69c26e275deef4de
Rafi Siddique <rafisiddique67@gmail.com>
2025-12-18 19:13:42 +0600

    [Done] create users service

---
+++ src/app/modules/users/users.service.ts
@@ -0,0 +1,12 @@
+import { IUser } from './users.interface'
+import { User } from './users.model'
+
+const createUser = async (user: IUser): Promise<IUser | null> => {
+  const createdUser = await User.create(user)
+  if (!createdUser) {
+    throw new Error('Failed to create user')
+  }
+  return createdUser
+}
+
+export default { createUser }
