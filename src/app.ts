470c9704072ebc0a37848b28142300e75ad2121e
Rafi Siddique <rafisiddique67@gmail.com>
2025-12-19 11:01:27 +0600

    [Done] create users route and controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import router from './app/modules/users/users.route'
 
 const app: Application = express()
 
@@ -9,7 +10,16 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Application  route
+app.use('/api/v1/users/', router)
+
+// testing
+app.get('/', async (req: Request, res: Response) => {
+  // await usersService.createUser({
+  //   id: '999',
+  //   password: '1234',
+  //   role: 'student',
+  // })
   res.send('Hello World!')
 })
 
--- src/app/modules/users/users.utils.ts
+++ src/app/modules/users/users.utils.ts
@@ -11,6 +11,6 @@ export const findLastUserId = async () => {
 
 export const generateUserId = async () => {
   const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
-
-  return currentId
+  const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return incrementedId
 }
