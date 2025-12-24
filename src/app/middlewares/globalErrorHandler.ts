324c7e71fbb0427b955ed9ff35cc5301d6e56e3b
Rafi Siddique <rafisiddique67@gmail.com>
2025-12-24 21:15:13 +0600

    [Done] make common error pattern for frontend

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,13 +1,21 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response } from 'express'
+import config from '../../config'
+import { IGenericErrorMessage } from '../../interfaces/error'
 
-const globalErrorHandler = (
-  err,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  res.status(400).json({ error: err })
-  next()
+const globalErrorHandler = (err, req: Request, res: Response) => {
+  // res.status(400).json({ error: err })
+  // next()
+
+  const statusCode = 500
+  const message = 'Something went wrong!'
+  const errorMessages: IGenericErrorMessage[] = []
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
+  })
 }
 
 export default globalErrorHandler
