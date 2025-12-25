df8a557fdf0433c3bb9e19d9a6b988636ff162d6
Rafi Siddique <rafisiddique67@gmail.com>
2025-12-25 19:58:03 +0600

    [Done] create validation error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,14 +1,50 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import config from '../../config'
 import { IGenericErrorMessage } from '../../interfaces/error'
+import handleValidationError from '../../errors/handleValidationError'
+import { error } from 'winston'
+import ApiError from '../../errors/ApiError'
 
-const globalErrorHandler = (err, req: Request, res: Response) => {
+const globalErrorHandler = (
+  err,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   // res.status(400).json({ error: err })
   // next()
 
-  const statusCode = 500
-  const message = 'Something went wrong!'
-  const errorMessages: IGenericErrorMessage[] = []
+  let statusCode = 500
+  let message = 'Something went wrong!'
+  let errorMessages: IGenericErrorMessage[] = []
+
+  if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  }
 
   res.status(statusCode).json({
     success: false,
@@ -16,6 +52,8 @@ const globalErrorHandler = (err, req: Request, res: Response) => {
     errorMessages,
     stack: config.env !== 'production' ? err?.stack : undefined,
   })
+
+  next()
 }
 
 export default globalErrorHandler
