08119591d8a20178c787c26c573f51f5aabd0ed4
Rafi Siddique <rafisiddique67@gmail.com>
2026-01-05 20:58:46 +0600

    [Done] implement dynamic filtering

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -6,6 +6,7 @@ import httpStatus from 'http-status'
 import pick from '../../../shared/pick'
 import { paginationFields } from '../../../constants/pagination'
 import { IAcademicSemester } from './academicSemester.interface'
+import { academicSemesterFilterableFields } from './academicSemester.constant'
 
 const createAcademicSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -25,7 +26,7 @@ const createAcademicSemester = catchAsync(
 )
 
 const getAllSemesters = catchAsync(async (req: Request, res: Response) => {
-  const filters = pick(req.query, ['searchTerm'])
+  const filters = pick(req.query, academicSemesterFilterableFields)
   const paginationOptions = pick(req.query, paginationFields)
 
   const result = await AcademicSemesterService.getAllSemesters(
