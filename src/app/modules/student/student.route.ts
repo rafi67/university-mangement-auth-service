d4f15dbb988f261112b17eac275c0d311d0cc611
Rafi Siddique <rafisiddique67@gmail.com>
2026-01-31 14:08:05 +0600

    [Done] create update route, student validation

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import { StudentController } from './student.controller'
+import validateRequest from '../../middlewares/validateRequest'
+import { StudentValidation } from './student.validation'
 
 const router = express.Router()
 
@@ -7,6 +9,10 @@ router.get('/:id', StudentController.getSingleStudent)
 router.get('/', StudentController.getAllStudents)
 router.delete('/:id', StudentController.deleteStudent)
 
-// router.patch('/create-student')
+router.patch(
+  '/update-student/:id',
+  validateRequest(StudentValidation.updateStudentZodSchema),
+  StudentController.updateStudent,
+)
 
 export const StudentRoutes = router
