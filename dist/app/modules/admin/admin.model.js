e6f7df962d43c4dac48c23477bb96a8477f9e079
Rafi Siddique <rafisiddique67@gmail.com>
2026-02-13 17:55:51 +0600

    [Done] production file rebuild

---
+++ dist/app/modules/admin/admin.model.js
@@ -0,0 +1,80 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Admin = exports.adminSchema = void 0;
+const mongoose_1 = require("mongoose");
+const admin_constant_1 = require("./admin.constant");
+exports.adminSchema = new mongoose_1.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: {
+            firstName: {
+                type: String,
+                required: true,
+            },
+            middleName: {
+                type: String,
+            },
+            lastName: {
+                type: String,
+                required: true,
+            },
+        },
+    },
+    dateOfBirth: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    contactNo: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    emergencyContactNo: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        enum: admin_constant_1.gender,
+        required: true,
+    },
+    permanentAddress: {
+        type: String,
+        required: true,
+    },
+    presentAddress: {
+        type: String,
+        required: true,
+    },
+    bloodGroup: {
+        type: String,
+        enum: admin_constant_1.bloodGroup,
+    },
+    managementDepartment: {
+        type: String,
+        ref: 'ManagementDepartment',
+        required: true,
+    },
+    designation: {
+        type: String,
+        required: true,
+    },
+    profileImage: {
+        type: String,
+    },
+}, {
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+    },
+});
+exports.Admin = (0, mongoose_1.model)('Admin', exports.adminSchema);
---
+++ dist/app/modules/student/student.validation.js
@@ -0,0 +1,52 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.StudentValidation = void 0;
+const zod_1 = __importDefault(require("zod"));
+const student_constant_1 = require("./student.constant");
+const updateStudentZodSchema = zod_1.default.object({
+    body: zod_1.default.object({
+        name: zod_1.default
+            .object({
+            firstName: zod_1.default.string().optional(),
+            middleName: zod_1.default.string().optional(),
+            lastName: zod_1.default.string().optional(),
+        })
+            .optional(),
+        dateOfBirth: zod_1.default.string().optional(),
+        gender: zod_1.default.enum([...student_constant_1.gender]).optional(),
+        bloodGroup: zod_1.default.enum([...student_constant_1.bloodGroup]).optional(),
+        email: zod_1.default.email().optional(),
+        contactNo: zod_1.default.string().optional(),
+        emergencyContactNo: zod_1.default.string().optional(),
+        presentAddress: zod_1.default.string().optional(),
+        permanentAddress: zod_1.default.string().optional(),
+        academicSemester: zod_1.default.string().optional(),
+        academicDepartment: zod_1.default.string().optional(),
+        academicFaculty: zod_1.default.string().optional(),
+        guardian: zod_1.default
+            .object({
+            fatherName: zod_1.default.string().optional(),
+            fatherOccupation: zod_1.default.string().optional(),
+            fatherContactNo: zod_1.default.string().optional(),
+            motherName: zod_1.default.string().optional(),
+            motherOccupation: zod_1.default.string().optional(),
+            address: zod_1.default.string().optional(),
+        })
+            .optional(),
+        localGuardian: zod_1.default
+            .object({
+            name: zod_1.default.string().optional(),
+            occupation: zod_1.default.string().optional(),
+            contactNo: zod_1.default.string().optional(),
+            address: zod_1.default.string().optional(),
+        })
+            .optional(),
+        profileImage: zod_1.default.string().optional(),
+    }),
+});
+exports.StudentValidation = {
+    updateStudentZodSchema,
+};
